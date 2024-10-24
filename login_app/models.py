e0a04d1bfbf27d326f795c107c869bb379c77a87
agus-tito <titoagustinsuarez6@gmail.com>
2024-10-24 16:00:41 -0300

    jonchi rico

--- login_app/models.py
+++ login_app/models.py
@@ -1,19 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-class Category(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.nombre
-
-class Product(models.Model):
-    nombre = models.CharField(max_length=200)
-    descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
-    categoria = models.ForeignKey(Category, on_delete=models.CASCADE)
-    imagen = models.ImageField(upload_to='products/', blank=True, null=True)
-
-    def __str__(self):
-        return self.nombre
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
