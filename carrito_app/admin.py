586c326873feb866a3011e2624f811eb39e2307c
AlejoMontuori <Alejomontuori12@gmail.com>
2024-10-25 18:22:50 -0300

    admin integrado

--- carrito_app/admin.py
+++ carrito_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Cart, CartItem, Order, OrderItem
 
 # Register your models here.
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+        list_display = [field.name for field in Cart._meta.fields]  # Mostrar todas las columnas
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+        list_display = [field.name for field in CartItem._meta.fields]  # Mostrar todas las columnas
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+        list_display = [field.name for field in Order._meta.fields]  # Mostrar todas las columnas
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+        list_display = [field.name for field in OrderItem._meta.fields]  # Mostrar todas las columnas
--- catalogo_app/admin.py
+++ catalogo_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+# Registrar el modelo Producto
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+        search_fields = ('nombre',)  # Búsqueda por nombre
+        list_filter = ('precio', 'stock')  # Filtro por precio y stock
+        ordering = ('nombre',)  # Ordenar por nombre    
+        list_display = [field.name for field in Product._meta.fields]  # Mostrar todas las columnas
+
+# Registrar el modelo Category
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ('nombre',)  # Búsqueda por nombre
+    list_filter = ('nombre',)  # Filtro por nombre (la categoría no suele tener precio o stock)
+    ordering = ('nombre',)  # Ordenar por nombre    
+    list_display = [field.name for field in Category._meta.fields]  # Mostrar todas las columnas
