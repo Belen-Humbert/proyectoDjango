6054f9d5e3119d8ed541024a7e4dba50944ca3d8
AlejoMontuori <Alejomontuori12@gmail.com>
2024-10-28 20:31:21 -0300

    login funcionando booooca

--- crud_app/views.py
+++ crud_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
-
+from django.contrib.auth import authenticate, login
 
 def registrarUsuario(request):
     if request.method == 'POST':
@@ -10,5 +10,26 @@ def registrarUsuario(request):
         password = request.POST['password1']
         # Crear y guardar el user
 
-        User.objects.create(username = username, email = email, password = password)
+        User.objects.create_user(username = username, email = email, password = password)
         return redirect('/')
+    
+
+def verificarUser(request):
+    if request.method == 'POST':
+        nombre = request.POST['username']
+        contraseña = request.POST['password']
+
+        # Autenticar al usuario
+        user = authenticate(request, username=nombre, password=contraseña)
+
+        if user is not None:  # Si la autenticación fue exitosa
+            login(request, user)  # Este método inicia sesión en el sistema y establece una sesión para el usuario, lo que permite que el usuario esté autenticado en futuras peticiones.
+            return redirect('/')  # Redirige a la página principal
+        else:
+            # Si la autenticación falla se muestra mensaje de error
+            return render(request, 'login_app/login.html', {
+                'error_message': 'Nombre o contraseña incorrecta'
+            })
+    
+     # Si la solicitud no es POST, redirigir o renderizar una página de error o de login
+    return render(request, 'login_app/registro.html')
