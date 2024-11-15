675d2a05b2403d905b6562a761d8db02d2bbbd54
agus-tito <titoagustinsuarez6@gmail.com>
2024-11-14 21:27:25 -0300

    feat: agregando templates de footer, agregando categorias en catalogo.

--- catalogo_app/views.py
+++ catalogo_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Product
+from .models import Category
 
 def catalogo_view(request):
     productos = Product.objects.all()
-    return render(request, 'catalogo_app/catalogo.html', {'productos': productos})
+    categorias = Category.objects.all()
+    return render(request, 'catalogo_app/catalogo.html', {'productos': productos, 'categorias' : categorias})
