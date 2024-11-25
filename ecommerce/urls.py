4c2982ca66b83a686ec1800e46c8f1cae775cae1
luana <luanacanselmocarla@gmail.com>
2024-11-25 15:14:17 -0300

    arreglos front. compra, mensaje de confirmación, estilos en home, se muestra img en catalogo, se agrega login en español, se agrega carrusel

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -9,5 +11,8 @@ urlpatterns = [
     path('catalogo/', include('catalogo_app.urls')),
     path('carrito/', include('carrito_app.urls')),
     path('crud/', include('crud_app.urls')),
-
 ]
+
+# Servir archivos de medios durante el desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
